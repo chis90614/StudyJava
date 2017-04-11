c360773afdfc76cfd74e915c17f693e462c3d426
chis90614 <sims2favorit@naver.com>
2017-04-11 09:48:17 +0900

    Package Test

---
+++ JP_170411/src/com/java/hello/Hello.java
@@ -0,0 +1,7 @@
+package com.java.hello;
+
+public class Hello {
+	public void helloMethod(){
+		System.out.println("안녕하세요!");
+	}
+}
---
+++ JP_170411/src/com/java/main/HelloTest.java
@@ -0,0 +1,10 @@
+package com.java.main;
+import com.java.hello.Hello;
+
+public class HelloTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		Hello h =  new Hello();
+		h.helloMethod();
+	}
+}
